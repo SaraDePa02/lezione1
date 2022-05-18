f34bfd0b68debe72e359199c2e3714e5e57c9892
Sara De Paoli <saradepaoli02@gmail.com>
2022-05-18 10:12:02 +0200

    change1

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+import {somma} from "./math.js";
+
 console.log("script start");    
+let msg = 'ciao';
+
+const pi =3.14; 
+
+if(msg === 'ciao'){
+    console.log('il messaggio è ciao');
+} 
+else{
+    console.log("il messaggio non contiene ciao")
+}
+
+for(let i =0; i<10;i++){
+    console.log(i);
+}
+
+let i=0;
+while(i<10){
+    console.log("--" + i);
+    i+=1;
+}
+
+
+
+const somma1 = (n1,n2) => {
+    let res = n1 + n2; 
+    return res; 
+}
+console.log('somma1: ' + somma1(15,10));
---
+++ math.js
@@ -0,0 +1,10 @@
+const somma = (n1,n2) => {
+    let res = n1 + n2; 
+    return res; 
+}
+
+const sottr = (n1,n2)=> {
+    let res = n1 - n2;
+    return res;
+}
+export {somma,sottr};
